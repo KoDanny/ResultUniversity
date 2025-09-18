50b063f8916e2ec1a0a425601b895e5e390b0111
Denis <124502391+KoDanny@users.noreply.github.com>
2025-09-18 13:42:03 +0300

    chore: установка react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = document.querySelector('#root')! as HTMLElement;
 
-createRoot(root).render(<App />);
+createRoot(root).render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
+);
