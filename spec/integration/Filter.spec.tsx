c6ad08205d8d9052ade865fa0276ffcd26e6c603
Denis <124502391+KoDanny@users.noreply.github.com>
2026-01-12 15:54:00 +0300

    test: добавление тестов

--- spec/integration/Filter.spec.tsx
+++ spec/integration/Filter.spec.tsx
@@ -1,9 +1,82 @@
+import { cleanup, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { TaskList } from 'src/modules/TaskList';
+import { createTestProvider } from '../utils/createTestProvider';
+
 describe('Список задач', () => {
-    // не содержит выполненные задачи
-    // после нажатия на кнопку фильтрации
-    it.todo('с включенным фильтром');
-    
-    // показывает как выполненные, так и не выполненные задачи
-    // после повторного нажатия на кнопку фильтрации
-    it.todo('с выключенным фильтром');
+	const user = userEvent.setup({
+		advanceTimers: jest.advanceTimersByTime,
+	});
+
+	beforeEach(() => {
+		cleanup();
+	});
+
+	// не содержит выполненные задачи
+	// после нажатия на кнопку фильтрации
+	it('с включенным фильтром', async () => {
+		const mockTasks = [
+			{ id: '1', header: 'Выполненная задача', done: true },
+			{ id: '2', header: 'Невыполненная задача', done: false },
+			{ id: '3', header: 'Выполненная задача', done: true },
+		];
+
+		render(<TaskList />, {
+			wrapper: createTestProvider(mockTasks),
+		});
+
+		const filterEl = screen.getByRole('checkbox', {
+			name: /Не показывать выполненные/i,
+		});
+
+		await user.click(filterEl);
+		expect(filterEl).toBeChecked();
+
+		const allTasks = screen.getAllByText(/задача/i);
+
+		allTasks.forEach(({ textContent }) => {
+			expect(textContent).toBe('Невыполненная задача');
+		});
+	});
+
+	// показывает как выполненные, так и не выполненные задачи
+	// после повторного нажатия на кнопку фильтрации
+	it('с выключенным фильтром', () => {
+		const mockTasks = [
+			{ id: '1', header: 'Выполненная задача', done: true },
+			{ id: '2', header: 'Невыполненная задача', done: false },
+			{ id: '3', header: 'Выполненная задача', done: true },
+		];
+
+		render(<TaskList />, {
+			wrapper: createTestProvider(mockTasks),
+		});
+
+		const filterEl = screen.getByRole('checkbox', {
+			name: /Не показывать выполненные/i,
+		});
+
+		expect(filterEl).not.toBeChecked();
+
+		const allTasks = screen.getAllByText(/задача/i);
+
+		allTasks.forEach((task) => {
+			expect(task).toBeInTheDocument();
+		});
+	});
+
+	it('Пустой список задач', () => {
+		render(<TaskList />, {
+			wrapper: createTestProvider([]),
+		});
+
+		const filterEl = screen.queryByRole('checkbox', {
+			name: /Не показывать выполненные/i,
+		});
+
+		const emptyEl = screen.getByText(/вы пока не создали/i);
+
+		expect(filterEl).toBeNull();
+		expect(emptyEl).toBeInTheDocument();
+	});
 });
--- spec/unit/Item.spec.tsx
+++ spec/unit/Item.spec.tsx
@@ -1,5 +1,85 @@
+import { render } from '@testing-library/react';
+import { Item } from 'src/components/Item';
+
 describe('Элемент списка задач', () => {
-    it.todo('название не должно быть больше 32 символов');
-    it.todo('название не должно быть пустым');
-    it.todo('нельзя удалять невыполненные задачи');
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
+	it.each([
+		{
+			header: '',
+			description: 'Невалидный: Пустой заголовок',
+		},
+		{
+			header: 'A'.repeat(33),
+			description: 'Невалидный: Длина заголовка равна 33 символам',
+		},
+		{
+			header: 'A'.repeat(50),
+			description: 'Невалидный: Длина заголовка больше 32 символов',
+		},
+		{
+			header: '1',
+			description: 'Валидный: Длина заголовка равна 1 символу',
+		},
+		{
+			header: 'Нормальный заголовок',
+			description: 'Валидный: Длина заголовка меньше 32 символов',
+		},
+		{
+			header: 'A'.repeat(32),
+			description: 'Валидный: Длина заголовка равна 32 символам',
+		},
+	])('$description', ({ header }) => {
+		const itemProps = {
+			id: 'test',
+			header,
+			done: false,
+			onDelete: jest.fn(),
+			onToggle: jest.fn(),
+		};
+
+		const { queryByRole } = render(<Item {...itemProps} />);
+
+		const taskItemEl = queryByRole('listitem');
+
+		if (header.length <= 32 && header.length > 0) {
+			expect(taskItemEl).toBeInTheDocument();
+		} else {
+			expect(taskItemEl).toBeNull();
+		}
+	});
+
+	it.each([false, true])('Задача выполнена(done): %s', (done) => {
+		const itemProps = {
+			id: 'test',
+			header: 'заголовок',
+			done,
+			onDelete: jest.fn(),
+			onToggle: jest.fn(),
+		};
+
+		const { asFragment } = render(<Item {...itemProps} />);
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it('Удалить можно только выполненную задачу', () => {
+		const itemProps = {
+			id: 'test',
+			header: 'заголовок',
+			onDelete: jest.fn(),
+			onToggle: jest.fn(),
+		};
+
+		const { rerender, getByRole } = render(
+			<Item {...itemProps} done={false} />
+		);
+
+		expect(getByRole('button')).toBeDisabled();
+
+		rerender(<Item {...itemProps} done={true} />);
+
+		expect(getByRole('button')).not.toBeDisabled();
+	});
 });
